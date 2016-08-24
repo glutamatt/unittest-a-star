dc8898755d9f3124af737cc302d0942a2187c454
glutamatt <glutamatt@live.fr>
2016-08-24 18:24:37 +0200

    Some maze methods tests

--- tests/MazeTest.php
+++ tests/MazeTest.php
@@ -5,7 +5,17 @@ class MazeTest extends \PHPUnit\Framework\TestCase
     public function testNewInstance()
     {
         $maze = new Maze();
-
         $this->assertInstanceOf('Maze', $maze);
+    }
+
+    public function testMethod()
+    {
+        $m = new Maze();
+        $m->setStartNode(23);
+        $this->assertEquals(23 , $m->getStartIndex());
+
+        $m->setGoalNode(55);
+
+        $this->assertEquals(55, $m->getGoalIndex());
     }
 }
