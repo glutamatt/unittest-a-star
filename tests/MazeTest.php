27c7c22b76b48b97034b8faafac9dd2bf0161a4e
glutamatt <glutamatt@live.fr>
2016-08-25 07:58:08 +0200

    Fixed tests

--- tests/MazeTest.php
+++ tests/MazeTest.php
@@ -14,7 +14,7 @@ class MazeTest extends \PHPUnit\Framework\TestCase
         $m->setStartNode(23);
         $this->assertEquals(23 , $m->getStartIndex());
 
-        $m->setGoalNode(53);
+        $m->setGoalNode(55);
 
         $this->assertEquals(55, $m->getGoalIndex());
     }
