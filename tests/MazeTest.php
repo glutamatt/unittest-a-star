972ea3cb9070d2a2e1e2339b6c7d6ffdbeebf7a3
glutamatt <glutamatt@live.fr>
2016-08-25 08:38:27 +0200

    tests dependencies with @depends

--- tests/MazeTest.php
+++ tests/MazeTest.php
@@ -6,16 +6,19 @@ class MazeTest extends \PHPUnit\Framework\TestCase
     {
         $maze = new Maze();
         $this->assertInstanceOf('Maze', $maze);
+
+        return $maze;
     }
 
-    public function testMethod()
+    /**
+     * @param Maze $m
+     * @depends testNewInstance
+     */
+    public function testMethod(Maze $m)
     {
-        $m = new Maze();
         $m->setStartNode(23);
         $this->assertEquals(23 , $m->getStartIndex());
-
         $m->setGoalNode(55);
-
         $this->assertEquals(55, $m->getGoalIndex());
     }
 }
